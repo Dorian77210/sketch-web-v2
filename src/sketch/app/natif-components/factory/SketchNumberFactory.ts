0d8ae67cfab5132f09b5cbe97bafae8ebf94b816
Dorian Terbah <dorian.terb@gmail.com>
2023-02-20 21:54:19 +0100

    Add factory to serialize/stringify SketchNumberInput

---
+++ src/sketch/app/natif-components/factory/SketchNumberFactory.ts
@@ -0,0 +1,30 @@
+import { SketchNumberInputComponent } from "../SketchNumberInputComponent";
+
+import SketchComponentFactory from "@/sketch/api/factory/SketchComponentFactory";
+
+interface SketchNumberInputComponentJSON {
+    value: number;
+} 
+
+export default class SketchNumberFactory implements SketchComponentFactory<SketchNumberInputComponent> {
+
+    fromJSON(rawJson: string) : SketchNumberInputComponent {
+        const component = new SketchNumberInputComponent();
+        const inputComponentJSON: SketchNumberInputComponentJSON = JSON.parse(rawJson);
+        component.setValue(inputComponentJSON.value);
+        return component;
+    }
+
+    toJSON(component: SketchNumberInputComponent) : object {
+        let nbr = component.getValue();
+        if (nbr === undefined) {
+            nbr = 0;
+        }
+
+        const json: SketchNumberInputComponentJSON = {
+            value: nbr
+        };
+
+        return json;
+    }
+}
---
+++ tests/unit/natif-components/factory/SketchNumberFactory.spec.ts
@@ -0,0 +1,32 @@
+import { expect } from 'chai'
+
+import SketchNumberFactory from '@/sketch/app/natif-components/factory/SketchNumberFactory';
+import { SketchNumberInputComponent } from '@/sketch/app/natif-components/SketchNumberInputComponent';
+
+const factory = new SketchNumberFactory();
+let component: SketchNumberInputComponent;
+
+describe('Test the factory for the SketchNumberComponent', () => {
+    it('Parse a SketchNumberComponent from a valid JSON', () => {
+        component = factory.fromJSON(JSON.stringify({ value: 10 }));
+        expect(component.getValue()).equals(10);
+    });
+
+    it('Parse a SketchNumberComponent from a JSON without number value', () => {
+        component = factory.fromJSON(JSON.stringify({}));
+        expect(component.getValue()).to.be.undefined;
+    });
+
+    it('Serialize a SketchNumberComponent with number set', () => {
+        component = new SketchNumberInputComponent();
+        component.setValue(100);
+        const json = factory.toJSON(component);
+        expect(json).deep.equals({ value: 100 });
+    });
+
+    it('Serialize a SketchNumberComponent with none number set', () => {
+        component = new SketchNumberInputComponent();
+        const json = factory.toJSON(component);
+        expect(json).deep.equals({ value: 0 });
+    });
+});
