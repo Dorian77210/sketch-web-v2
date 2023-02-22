b88912effc7d3b20123914e5d27c7a6dab1d12c0
Dorian Terbah <dorian.terb@gmail.com>
2023-02-23 00:37:51 +0100

    Begin new system of data injection in component

--- src/sketch/api/component-configuration.ts
+++ src/sketch/api/component-configuration.ts
@@ -1,5 +1,13 @@
 import { Class } from './types';
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import SketchComponent from './sketch-component';
+
+/**
+ * Interface used to inject data
+ */
+export interface MethodInjectable<S extends SketchComponent<unknown>, T> {
+    (component: S, data: T): void;
+}
 
 export type ComponentSlotConfiguration = {
     entryName: string;
